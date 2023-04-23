1083efef763842be33197c49e9234d8394c142de
mmichaelnorward777 <67016919+mmichaelnorward777@users.noreply.github.com>
2023-04-23 21:35:09 +0800

    Update create-app-window.js

--- core/browser/api/create-app-window.js
+++ core/browser/api/create-app-window.js
@@ -2,17 +2,17 @@ const AppWindow = require("../classes/app-window");
 
 module.exports = function (resourceLocation) {
     
-
     let mainAppWindow;
 
     mainAppWindow = new AppWindow(resourceLocation);
 
     mainAppWindow.initialize();
 
-    // mainAppWindow.windowObject.webContents.openDevTools();
+    mainAppWindow.windowObject.webContents.openDevTools();
 
     mainAppWindow.windowObject.webContents.on("did-finish-load", (e) => {
 
+        // we add the windowObject to the global and static properties of the class that created them
         mainAppWindow.addToWindowObjects();
         
         mainAppWindow.windowObject.webContents.send("app-window-id", mainAppWindow.windowId);        
