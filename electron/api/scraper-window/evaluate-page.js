e034d99565087ad1ddf4a667ff65c7450572c92e
mmichaelnorward777 <67016919+mmichaelnorward777@users.noreply.github.com>
2023-07-24 10:03:14 +0800

    Update evaluate-page.js

--- electron/api/scraper-window/evaluate-page.js
+++ electron/api/scraper-window/evaluate-page.js
@@ -44,10 +44,10 @@ module.exports = async function({ ccScraperWindow, resourceUri, dataObject, uriP
         }
 
         if(noredirect)  {
-            ccScraperWindow.windowObject.webContents.on("will-redirect", preventDefaultFunction);
+            ccScraperWindow.windowObject.webContents.once("will-redirect", preventDefaultFunction);
         }
 
-        ccScraperWindow.windowObject.webContents.on("did-finish-load", async (e) => {
+        ccScraperWindow.windowObject.webContents.once("did-finish-load", async (e) => {
 
             // cookie session
             session.defaultSession.cookies.set({url: 'https://www.google.com', name: 'cookieName', value: 'cookieValue', domain: '.google.com'});
@@ -62,9 +62,9 @@ module.exports = async function({ ccScraperWindow, resourceUri, dataObject, uriP
             }     
             
 
-            ccScraperWindow.windowObject.webContents.on("will-navigate", preventDefaultFunction);
+            ccScraperWindow.windowObject.webContents.once("will-navigate", preventDefaultFunction);
         
-            ccScraperWindow.windowObject.webContents.on('did-start-loading', preventDefaultFunction);
+            ccScraperWindow.windowObject.webContents.once('did-start-loading', preventDefaultFunction);
 
             ccScraperWindow.windowObject.webContents.session.webRequest.onCompleted((details) => {
                 // console.log({
