f0509a7a2630471af795bfe66858541b90189248
mmichaelnorward777 <67016919+mmichaelnorward777@users.noreply.github.com>
2023-08-22 03:33:32 +0800

    Update index.js

--- test/scripts/office-depot/index.js
+++ test/scripts/office-depot/index.js
@@ -13,7 +13,7 @@ module.exports = async function(app, ipcMain)   {
 
         // await categorizedSetScraper(app, ipcMain);
 
-        await productSetScraper(app, ipcMain, 43);
+        await productSetScraper(app, ipcMain, 44);
 
         // await singleProductScraper(app, ipcMain, 9749);
 
